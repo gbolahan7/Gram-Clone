8511f6ddedc6fc0613403557756ed41f2c02c8bf
Abass <abass_gbolahan@yahoo.com>
2022-01-21 10:35:29 +0000

    Front end cleanup

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -13,9 +14,30 @@ class ProfilesController extends Controller
     {
     //    $user = User::findOrFail($user);
     $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+    
+    $postCount = Cache::remember(
+                'count.posts.' . $user->id, //key used for the cache
+                 now()->addSeconds(30), // store it for 30sec
+                function() use ($user){
+                    return $user->posts->count();
+                });
+
+    $followersCount = Cache::remember(
+                    'count.followers.' . $user->id, //key used for the cache
+                    now()->addSeconds(30), // store it for 30sec
+                    function() use ($user){
+                        return $user->profile->followers->count();
+               });
+
+    $followingCount = Cache::remember(
+                    'count.following.' . $user->id, //key used for the cache
+                    now()->addSeconds(30), // store it for 30sec
+                    function() use ($user){
+                        return $user->following->count();
+                });
 
         
-        return view('profiles.index', compact('user', 'follows'));
+        return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
